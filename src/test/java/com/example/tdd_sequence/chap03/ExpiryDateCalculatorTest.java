54e8e673e57496c966e4cbe2d5720af373f7e6e1
YINAKIM <yinaofficeg@gmail.com>
2022-07-08 10:33:01 +0900

    [step4] 예외상황1

--- src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
@@ -9,15 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달뒤가_만료일이_됨(){
+        // case 1
         assertExpiryDate(
                 LocalDate.of(2019,3,1),
                 10_000,
                 LocalDate.of(2019,4,1)
         );
+
+        // case 2
         assertExpiryDate(
                 LocalDate.of(2019,5,5),
                 10_000,
                 LocalDate.of(2019,6,5)
+        );
+
+        // case 3 : 납부일자 != 한달뒤일자 (불일치하는 경우)
+        assertExpiryDate(
+                LocalDate.of(2019,1,31),
+                10_000,
+                LocalDate.of(2019,2,28)
         );
 
     }
@@ -27,5 +37,15 @@ public class ExpiryDateCalculatorTest {
         ExpiryDateCalCulator cal = new ExpiryDateCalCulator();
         LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
         assertEquals(expectedExpiryDate, realExpiryDate);
+    }
+
+    @Test
+    void 납부일과_한달뒤_일자가_같지않은_예외상황(){
+        assertExpiryDate(
+                LocalDate.of(2019,1,31),
+                10_000,
+                LocalDate.of(2019,2,28)
+        );
+        // pass : LocalDate.plusMonths(1); 로 알아서 한달추가 처리를 해줬음
     }
 }
