fe3b2937dc7401d6277ec1002804389d0510b495
YINAKIM <yinaofficeg@gmail.com>
2022-07-08 10:26:07 +0900

    [step3] 리팩토링 - 테스트메서드가 검증하는 내용이 무엇인지 명확히 확인 가능한 선에서

--- src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
@@ -9,23 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달뒤가_만료일이_됨(){
+        assertExpiryDate(
+                LocalDate.of(2019,3,1),
+                10_000,
+                LocalDate.of(2019,4,1)
+        );
+        assertExpiryDate(
+                LocalDate.of(2019,5,5),
+                10_000,
+                LocalDate.of(2019,6,5)
+        );
 
-        // [step1] 쉬운것부터 테스트 - 상수를 리턴해서 테스트 통과
-        LocalDate billingDate = LocalDate.of(2019,3,1);
-        int payAmount = 10_000;
+    }
 
+    // [step3] 리팩토링 - 테스트메서드가 검증하는 내용이 무엇인지 명확히 확인 가능한 선에서
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
         ExpiryDateCalCulator cal = new ExpiryDateCalCulator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2019,4,1), expiryDate);
-
-        // [step2] 예를 추가하면서 구현을 일반화
-        LocalDate billingDate2 = LocalDate.of(2019,5,5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalCulator cal2 = new ExpiryDateCalCulator();
-        LocalDate expiryDate2 = cal.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertEquals(LocalDate.of(2019,6,5), expiryDate2);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
