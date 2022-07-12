6e0f5b73a96ba1411fa98cd19293517584ed6f1d
YINAKIM <yinaofficeg@gmail.com>
2022-07-12 17:45:56 +0900

    [리팩토링] 파라미터 개수가 3개 이상이 되면 객체로 만들어서 사용하도록

--- src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tdd_sequence/chap03/ExpiryDateCalculatorTest.java
@@ -9,41 +9,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달뒤가_만료일이_됨(){
+
+
         // case 1
         assertExpiryDate(
-                LocalDate.of(2019,3,1),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019,3,1))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019,4,1)
         );
 
         // case 2
         assertExpiryDate(
-                LocalDate.of(2019,5,5),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019,5,5))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019,6,5)
         );
 
         // case 3 : 납부일자 != 한달뒤일자 (불일치하는 경우)
         assertExpiryDate(
-                LocalDate.of(2019,1,31),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019,1,31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019,2,28)
         );
 
     }
 
-    // [step3] 리팩토링 - 테스트메서드가 검증하는 내용이 무엇인지 명확히 확인 가능한 선에서
-    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+    // [step4] 리팩토링 - 파라미터가 3개이상이면 객체를 사용
+    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
+
         ExpiryDateCalCulator cal = new ExpiryDateCalCulator();
-        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
         assertEquals(expectedExpiryDate, realExpiryDate);
     }
 
     @Test
     void 납부일과_한달뒤_일자가_같지않은_예외상황(){
         assertExpiryDate(
-                LocalDate.of(2019,1,31),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019,1,31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019,2,28)
         );
         // pass : LocalDate.plusMonths(1); 로 알아서 한달추가 처리를 해줬음
